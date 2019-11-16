5ad6fa2806b87a1dcdb751d42a4ca75dc87590c9
Methembe <mmoyo414@loc.edu>
2019-11-16 02:08:41 -0600

    Initial Commit

--- app/src/main/java/com/mmoyo/mestogram/MainActivity.java
+++ app/src/main/java/com/mmoyo/mestogram/MainActivity.java
@@ -11,7 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ImageView; //
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
